08440abe2fb33e443415700ec237088b361a821e
Dahinkpie <dahinkpie@gmail.com>
2023-05-02 21:30:39 -0500

    Recursivize

--- portfolio-app/src/helpers/config_parser.js
+++ portfolio-app/src/helpers/config_parser.js
@@ -1,15 +1,20 @@
 export function home_config_parser(home_config) {
     let index = 0;
     home_config.articles.forEach((article, i) => {
-        //console.log(article)
-        article.cards.forEach((card, j) => {
-            home_config.articles[i].cards[j].id = index++;
-            if (card.cards) {
-                card.cards.forEach((card, z) => {
-                    home_config.articles[i].cards[j].cards[z].id = index++;
-                })
-            }
-        });
+        article.id = index++;
+        if (article.cards) {
+            iterateCards(article, index)
+        }
+    });
+}
+
+function iterateCards(card, index) {
+
+    card.cards.forEach((card) => {
+        card.id = index++;
+        if (card.cards) {
+            iterateCards(card, index);
+        }
     });
 }
 
@@ -17,20 +22,29 @@ export function map_cards(home_config) {
     home_config.articles.forEach((article, i) => {
         let map = new Map();
         article.cards.forEach(card => {
-            let subMap = new Map();
+
             map.set(card.id, card);
             if (card.cards) {
-                card.cards.forEach(card => {
-                    map.set(card.id, card)
-                    subMap.set(card.id, card);
-                });
-
-                card.cards = subMap
+                recursiveMapCards(card, map)
             }
         });
 
         home_config.articles[i].cards = map;
     });
+}
+
+function recursiveMapCards(card, map) {
+
+    let subMap = new Map();
+    card.cards.forEach(card => {
+        map.set(card.id, card)
+        subMap.set(card.id, card);
+        if (card.cards) {
+            recursiveMapCards(card, map, subMap)
+        }
+    });
+
+    card.cards = subMap
 }
 
 export function treemap(card) {
@@ -41,19 +55,12 @@ export function treemap(card) {
         let subMap = new Map();
 
         if (!added.has(card.id)) map.set(card.id, card);
-
-
         if (card.cards) {
             card.cards.forEach(card => {
-
                 added.add(card.id);
                 subMap.set(card.id, card);
             });
-
             card.cards = subMap
-
-
-
         }
     });
 
