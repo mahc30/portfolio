eb181dbc6cccce0a766ee81b74aca9b376157bdc
Dahinkpie <dahinkpie@gmail.com>
2023-10-07 19:54:02 -0500

    Idk

--- portfolio-app/src/components/play/ailens/Ailensweeper.jsx
+++ portfolio-app/src/components/play/ailens/Ailensweeper.jsx
@@ -2,11 +2,6 @@ import React from 'react'
 import p5 from 'p5';
 import './css/styles.css';
 import { sketch } from './aliensweeper';
-import alienImg from './assets/ailen8.png';
-import flagImg from './assets/ailen_flag.png';
-import restartImg1 from './assets/death2.png'
-import restartImg2 from './assets/alive1.png'
-import { Separador } from '../../atoms/Separador';
 
 export class Ailensweeper extends React.Component {
     constructor(props) {
